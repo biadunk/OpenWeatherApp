cc4d6976a0b304913f104783bdfbdd53a315bc2c
Kacper Biaduń <kacperbiadun.12@gmail.com>
2024-11-22 22:10:26 +0100

    wip

---
+++ OpenWeatherApp/OpenWeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OpenWeatherApp
+//
+//  Created by Kacper Biaduń on 22/11/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OpenWeatherApp/OpenWeatherApp/OpenWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenWeatherAppApp.swift
+//  OpenWeatherApp
+//
+//  Created by Kacper Biaduń on 22/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct OpenWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
